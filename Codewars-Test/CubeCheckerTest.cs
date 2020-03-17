c3db4db42b186cb5a98c9373fe9b139c9f933573
geotan <tanuk5@hotmail.com>
2020-03-17 21:46:01 +0100

    cube checker

---
+++ Codewars-Test/CubeCheckerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using System;
+
+namespace Codewars.Test
+{
+    [TestFixture]
+   public class CubeCheckerTest
+    {
+        [Test]
+        public void ShouldReturnTrue()
+        {
+            CubeChecker cube = new CubeChecker();
+            Assert.AreEqual(true, cube.IsCube(1, 1));
+            Assert.AreEqual(true, cube.IsCube(8, 2));            
+        }
+        [Test]
+        public void ShouldReturnFalse()
+        {
+            CubeChecker cube = new CubeChecker();
+            Assert.AreEqual(false, cube.IsCube(2, 1));
+            Assert.AreEqual(false, cube.IsCube(6, 3));           
+            Assert.AreEqual(false, cube.IsCube(0, 0));
+            Assert.AreEqual(false, cube.IsCube(200, 4));
+            Assert.AreEqual(false, cube.IsCube(-8, -2));
+        }
+    }
+}
+
---
+++ Codewars/CubeChecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    public class CubeChecker
+    {
+        public bool IsCube(double volume, double side)
+        {
+            return side <= 0 ? false : Math.Pow(side, 3) == volume;
+        }
+    }
+}
